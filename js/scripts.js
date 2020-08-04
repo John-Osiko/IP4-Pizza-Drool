5dbbcdce3b9737443c06e998ef1bafaf3e21b6d4
John-Osiko <johnmaxosiko@gmail.com>
2020-08-04 10:29:42 +0300

    Add a switch case function for orders in js

--- js/scripts.js
+++ js/scripts.js
@@ -24,3 +24,55 @@ $("button.proceed").click(function(event) {
             let order = (myflavour, mysize, mycrust, mysoda, mytopping, total) => {
                 return { myflavour, mysize, mycrust, mysoda, mytopping, total };
             };
+            switch (mysize) {
+                case "0":
+                    price = 0;
+                    break;
+                case "large":
+                    price = 1200;
+                    console.log(price);
+                    break;
+                case "medium":
+                    price = 850;
+                    console.log("The price is " + price);
+                    break;
+                case "small":
+                    price = 600;
+                    console.log(price);
+                default:
+                    console.log("error");
+            }
+            switch (mycrust) {
+                case "0":
+                    crust_price = 0;
+                    break;
+                case "Crispy":
+                    crust_price = 200;
+                    break;
+                case "Stuffed":
+                    crust_price = 250;
+                    break;
+                case "Gluten-free":
+                    crust_price = 180;
+                    break;
+                default:
+                    console.log("No price");
+            }
+            switch (mysoda) {
+                case "0":
+                    soda_price = 0;
+                    break;
+                case "water":
+                    soda_price = 120;
+                    console.log(soda_price);
+                    break;
+                case "soda":
+                    soda_price = 140;
+                    console.log("The price is " + soda_price);
+                    break;
+                case "juice":
+                    soda_price = 180;
+                    console.log(soda_price);
+                default:
+                    console.log("error");
+            }
