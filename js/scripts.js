e26974b56cbe10975318f8e1cd5edac817116f54
John-Osiko <johnmaxosiko@gmail.com>
2020-08-04 10:20:04 +0300

    Add a jquery function

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,19 @@ function pizza(flavour, size, crust, topping, soda, total) {
     this.topping = topping;
     this.soda = soda;
     this.total = total;
-}
+} //Get inputs
+$("button.proceed").click(function(event) {
+            let myflavour = $("#flavour option:selected").val();
+            let mysize = $("#size option:selected").val();
+            let mycrust = $("#crust option:selected").val();
+            let mysoda = $("#soda option:selected").val();
+            let mytopping = [];
+            $.each($("input[name='toppings']:checked"), function() {
+                mytopping.push($(this).val());
+            });
+            console.log(mytopping.join(", "));
+
+            //Function order
+            let order = (myflavour, mysize, mycrust, mysoda, mytopping, total) => {
+                return { myflavour, mysize, mycrust, mysoda, mytopping, total };
+            };
